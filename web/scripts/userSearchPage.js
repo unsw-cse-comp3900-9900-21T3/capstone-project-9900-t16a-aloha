eba5eff0c9b1a94f90275524cd4b286b13821869
walkm-an <bigloveharris@outlookcom>
2021-10-25 21:13:02 +1100

    Finish load more search page

--- web/scripts/userSearchPage.js
+++ web/scripts/userSearchPage.js
@@ -7,6 +7,8 @@ const productList = document.getElementById("user-search-page-products");
 // const addCartBtn = document.getElementById("user-addcart-product-btn");
 const loadMoreBtn = document.getElementById("search-load-more-btn");
 
+const selectBrand = document.getElementById("user-search-brand");
+const selectPrice = document.getElementById("user-search-price");
 let offset = 1;
 const getUserSearchProductList = async () => {
   removeAllChilds(productList);
@@ -215,4 +217,93 @@ loadMoreBtn.addEventListener("click", () => {
   offset++;
 });
 
+const priceBrand = async (filterIndex) => {
+  removeAllChilds(productList);
+  let url = `http://localhost:8080/test/user/search/?pageindex=${filterIndex}`;
+  if (selectBrand.value != "") {
+    url += "&brand=" + selectBrand.value;
+  }
+  if (selectPrice == 1) {
+    url += "&maxprice=" + "100";
+  } else if (selectPrice == 2) {
+    url += "&minprice=" + "101";
+    url += "&maxprice=" + "200";
+  } else if (selectPrice == 3) {
+    url += "&minprice=" + "200";
+  }
+  try {
+    const response = await fetch(url, {
+      method: "get",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const jsData = await response.text();
+    const d = JSON.parse(jsData);
+
+    console.log("Completed!", d);
+    if (d.status === "fail") {
+      alert("Something Wrong");
+    } else {
+      const items = d.content;
+      items.forEach((e) => {
+        const isExistP = document.getElementById(e.id);
+
+        if (e.isDeleted === 0 && !isExistP) {
+          const productTempNew = productTemplate.cloneNode(true);
+
+          productTempNew.id = e.id;
+
+          productTempNew.getElementsByClassName("card-text")[0].innerText =
+            e.name;
+          productTempNew.getElementsByClassName("badge")[0].innerText =
+            "$" + e.price;
+
+          const imgUrlArrays = JSON.parse(e.imgURL);
+          // console.log(imgUrlArrays);
+          if (e.imgURL == "[]" || !e.imgURL) {
+            productTempNew.getElementsByClassName("card-img-top")[0].src =
+              "assets/shoe.png";
+          } else {
+            productTempNew.getElementsByClassName("card-img-top")[0].src =
+              imgUrlArrays[0];
+          }
+
+          // wishlist  heart condition
+          const inWishList = getAllWishList();
+          inWishList.then((items) => {
+            items.forEach((p) => {
+              if (productTempNew.id == p.productID) {
+                productTempNew
+                  .getElementsByClassName("toggle-wishlist")[0]
+                  .classList.remove("bi-heart");
+                productTempNew
+                  .getElementsByClassName("toggle-wishlist")[0]
+                  .classList.add("bi-heart-fill");
+              }
+            });
+          });
+
+          // goto detail pages
+          productTempNew.addEventListener("click", () => {
+            gotoUserProductDetailPageDom(productTempNew.id);
+          });
+          productList.appendChild(productTempNew);
+        }
+      });
+      addWishListListener();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+selectBrand.addEventListener("change", () => {
+  filterIndex = 0;
+  priceBrand(filterIndex);
+});
+selectPrice.addEventListener("change", () => {
+  filterIndex = 0;
+  priceBrand(filterIndex);
+});
 export { getUserSearchProductList };
