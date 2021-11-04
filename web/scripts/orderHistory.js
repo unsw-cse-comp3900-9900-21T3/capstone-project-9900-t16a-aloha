56d0151472b14333e1d26d38d7f89cf0e05c9245
walkm-an <bigloveharris@outlookcom>
2021-11-04 15:20:09 +1100

    Add order history

--- web/scripts/orderHistory.js
+++ web/scripts/orderHistory.js
@@ -1,83 +1,91 @@
 import { removeAllChilds } from "./helper.js";
+import { getProductDetail } from "./gotoUserProductDetailPage.js";
 const orderItemParent = document.getElementById("order-history-list");
 const orderItemProductParentTemp =
   document.getElementById("order-history-item");
 
 const orderProductTemp = document.getElementById("order-product-item");
 
-const getAllOrders = () => {
+const getAllOrders = async () => {
   removeAllChilds(orderItemParent);
-  //   const url = `http://localhost:8080/test/user/${sessionStorage.getItem(
-  //     "userID"
-  //   )}/order`;
+  const url = `http://localhost:8080/test/getOrders/${sessionStorage.getItem(
+    "userID"
+  )}`;
   try {
-    // const response = await fetch(url, {
-    //   method: "get",
-    //   headers: {
-    //     "content-type": "application/json",
-    //   },
-    // });
-    // const jsData = await response.text();
-    // const d = JSON.parse(jsData);
-    // console.log("Completed!", d);
-    // if (d.status === "fail") {
-    //   alert("Something Wrong");
-    // } else {
-    // }
-    // for (let order in d.orders){
-
-    // }
-
-    let d = {
-      orderId: "abc",
-      phone: "423526851",
-      firstName: "Peng",
-      lastName: "Yang",
-      street: "2 Waterways st",
-      city: "WWP",
-      postcode: "2127",
-      state: "NSW",
-      products: [
-        {
-          id: 1,
-          size: 2,
-          qty: 3,
-        },
-        {
-          id: 2,
-          size: 2,
-          qty: 3,
-        },
-      ],
-    };
-    const anOrder = orderItemProductParentTemp.cloneNode(true);
-    anOrder.classList.add("order-history-item-card");
-    anOrder.getElementsByClassName("order-history-item-number")[0].innerText =
-      "Order #" + d.orderId;
-    anOrder.getElementsByClassName("order-history-item-name")[0].innerText =
-      d.firstName + " " + d.lastName;
-    anOrder.getElementsByClassName("order-history-item-addr")[0].innerText =
-      d.street;
-    anOrder.getElementsByClassName("order-history-item-phone")[0].innerText =
-      d.phone;
-    anOrder.getElementsByClassName("order-history-item-city")[0].innerText =
-      d.city;
-    anOrder.getElementsByClassName("order-history-item-state")[0].innerText =
-      d.state;
-    anOrder.getElementsByClassName("order-history-item-zip")[0].innerText =
-      d.postcode;
-    for (let p of d.products) {
-      const orderProduct = orderProductTemp.cloneNode(true);
-      orderProduct.id = d.orderId + "_" + p.id;
-      orderProduct.getElementsByClassName("shop-qty")[0].innerText = p.qty;
-      orderProduct.getElementsByClassName("shop-size")[0].innerText =
-        "US " + p.size;
+    const response = await fetch(url, {
+      method: "get",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const jsData = await response.text();
+    const orders = JSON.parse(jsData);
+    console.log("Completed! get orders", orders);
+    for (let orderIdx in orders) {
+      const d = orders[orderIdx];
+      console.log("order:", d);
+      const anOrder = orderItemProductParentTemp.cloneNode(true);
+      anOrder.removeAttribute("id");
+      anOrder.classList.add("order-history-item-card");
+      anOrder.getElementsByClassName("order-history-item-number")[0].innerText =
+        "Order #" + d.orderid;
+      anOrder.getElementsByClassName("order-history-item-name")[0].innerText =
+        d.firstName + " " + d.lastName;
+      anOrder.getElementsByClassName("order-history-item-addr")[0].innerText =
+        d.street;
+      anOrder.getElementsByClassName("order-history-item-phone")[0].innerText =
+        d.phone;
+      anOrder.getElementsByClassName("order-history-item-city")[0].innerText =
+        d.city;
+      anOrder.getElementsByClassName("order-history-item-state")[0].innerText =
+        d.state;
+      anOrder.getElementsByClassName("order-history-item-zip")[0].innerText =
+        d.postcode;
+      anOrder.getElementsByClassName("accordion-collapse")[0].id =
+        "order_" + d.orderid;
       anOrder
-        .getElementsByClassName("order-product-list")[0]
-        .appendChild(orderProduct);
-    }
+        .getElementsByClassName("accordion-button")[0]
+        .setAttribute("data-bs-target", "#" + "order_" + d.orderid);
+      let totalPrice = 0;
 
-    orderItemParent.appendChild(anOrder);
+      for (let p of d.products) {
+        const url = `http://localhost:8080/admin/search/id/${p.id}`;
+        const response = await fetch(url, {
+          method: "get",
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+
+        const jsData = await response.text();
+        const pDetail = JSON.parse(jsData);
+
+        const orderProduct = orderProductTemp.cloneNode(true);
+        orderProduct.id = d.orderid + "_" + p.id;
+        orderProduct.getElementsByClassName("shop-name")[0].innerText =
+          pDetail.name;
+        orderProduct.getElementsByClassName("shop-price")[0].innerText =
+          "$ " + pDetail.price;
+        orderProduct.getElementsByClassName("total-price")[0].innerText =
+          "$ " + parseFloat(pDetail.price) * parseInt(p.qty, 10);
+        totalPrice += parseFloat(pDetail.price) * parseInt(p.qty, 10);
+        orderProduct.getElementsByClassName("shop-qty")[0].innerText = p.qty;
+        orderProduct.getElementsByClassName("shop-size")[0].innerText =
+          "US " + p.size;
+        if (pDetail.imgURL != null && pDetail.imgURL != "[]") {
+          const imgsArrayJson = JSON.parse(pDetail.imgURL);
+          orderProduct.getElementsByClassName("order-product-item-img")[0].src =
+            imgsArrayJson[0];
+        }
+
+        anOrder
+          .getElementsByClassName("order-product-list")[0]
+          .appendChild(orderProduct);
+      }
+      anOrder.getElementsByClassName("order-total-price")[0].innerText =
+        "$" + totalPrice;
+      orderItemParent.appendChild(anOrder);
+    }
   } catch (err) {
     console.log(err);
   }
